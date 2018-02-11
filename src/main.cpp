218b67b66848f8fd017a4e72df5b8601601beabb
nlaurance <nicolas.laurance@gmail.com>
2018-02-11 16:11:58 +0000

    it works

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,43 @@
+//https://forum.arduino.cc/index.php?topic=331312.0
 #include <Arduino.h>
+#include <Wire.h>
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
 
+RH_ASK driver;
 
-
-
+// joystick
 const int xPin = A1;
 const int yPin = A0;
-const int buttonPin = 12;
-
-// propulsion
-const int motor1 = 6;
-const int motor2 = 7;
-const int PWMPropulsionPin = 5;
+const int buttonPin = 11;
 
 // direction
-const int motor3 = 8;
-const int motor4 = 9;
+const int NO_ORDER = 0;
+const int FORWARD = 1;
+const int BACKWARD = 2;
+const int TURN_RIGHT = 4;
+const int TURN_LEFT = 8;
 
+const int sensiAvant = 600;
+const int sensiArriere = 400;
+const int sensiDroite = 600;
+const int sensiGauche = 400;
+
+// variables
 int xPosition ;
 int yPosition ;
 int buttonState ;
 
 
-
-int sensiAvant = 600;
-int sensiArriere = 400;
-int sensiDroite = 600;
-int sensiGauche = 400;
-
-
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
-
   pinMode(buttonPin, INPUT);
   pinMode(xPin, INPUT);
   pinMode(yPin, INPUT);
 
-  pinMode(motor1, OUTPUT);
-  pinMode(motor2, OUTPUT);
-  pinMode(motor3, OUTPUT);
-  pinMode(motor4, OUTPUT);
-  pinMode(PWMPropulsionPin, OUTPUT);
-
+  Wire.begin(); // needed ?
+  Serial.begin(9600);
+  if (!driver.init())
+      Serial.println("TX init failed");
 }
 
 void loop() {
@@ -52,34 +48,47 @@ void loop() {
   int vitesseAvant = map(yPosition, 0, sensiAvant, 0, 255);
   int vitesseArriere = map(yPosition, sensiArriere, 0, 0, 255);
 
-  Serial.print("X: ");
-  Serial.print(xPosition);
-  Serial.print(" | Y: ");
-  Serial.print(yPosition);
-  Serial.print(" | Button: ");
-  Serial.println(buttonState);
+  int propulsion;
+  int vitesse;
+  int direction;
+  int braquage;
 
-  delay(100); // add some delay between reads
-
-  if (yPosition < sensiArriere) {
-    // backward
-    digitalWrite(motor1, HIGH);
-    digitalWrite(motor2, LOW);
-    analogWrite(PWMPropulsionPin, vitesseArriere);
-Serial.println("arriere");
+  if (vitesseAvant>0) {
+    propulsion=FORWARD;
+    vitesse=vitesseAvant;
+  }
+  else if (vitesseArriere>0) {
+    propulsion=BACKWARD;
+    vitesse=vitesseArriere;
+  }
+  else {
+    propulsion=NO_ORDER;
+    vitesse=0;
   }
 
-else if (yPosition > sensiAvant) {
-    // forward
-    digitalWrite(motor1, LOW);
-    digitalWrite(motor2, HIGH);
-    analogWrite(PWMPropulsionPin, vitesseAvant);
-Serial.println("avant");
-  }
-else {
-//   free wheel
-    digitalWrite(motor1, LOW);
-   digitalWrite(motor2, LOW);
-    Serial.println("roue libre");
- }
+  direction = TURN_RIGHT;
+
+  int instructions[4]; //avant/arriere, vitesse, droite/gauche, vitesse
+
+  // force for test
+  propulsion = 1;
+  vitesse = 128;
+  direction = 8;
+  braquage = 155;
+
+  instructions[0]=propulsion;
+  instructions[1]=vitesse;
+  instructions[2]=direction;
+  instructions[3]=braquage;
+
+  driver.send((uint8_t*)instructions,8); //8 == 4*sizeof(int)
+  driver.waitPacketSent(); // needed ?
+
+  Serial.println(instructions[0]);
+  Serial.println(instructions[1]);
+  Serial.println(instructions[2]);
+  Serial.println(instructions[3]);
+  Serial.println("-------SENT------------");
+
+  delay(1000);
 }
